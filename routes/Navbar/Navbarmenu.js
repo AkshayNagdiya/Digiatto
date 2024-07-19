67f8f3d1d4716759c10256efdf3f5017a9e64d23
yogesh gurjar <yogesh67gurjar@gmail.com>
2024-07-19 16:06:32 +0530

    navbar

--- routes/Navbar/Navbarmenu.js
+++ routes/Navbar/Navbarmenu.js
@@ -1,7 +1,7 @@
-// const express = require("express");
+const express = require("express");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/menu", upload.single("image"), handleNavbarLogoUpload);
+router.post("/menu", handleNavbarLogoUpload);
 
-// module.exports = router;
+module.exports = router;
