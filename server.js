d0ecd5eac72e4ff5a04c1540dca38b282b49941f
yogesh gurjar <yogesh67gurjar@gmail.com>
2024-07-19 16:10:24 +0530

    server

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // routes
-app.use("/navbar", require("./routes/Navbar"));
 app.use("/herosection", require("./routes/Herosectionroute"));
 app.use("/homeslider", require("./routes/Homeslider"));
 app.use("/whatwedo", require("./routes/Whatwedo"));
